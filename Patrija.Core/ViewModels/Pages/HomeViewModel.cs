cbce4015680da6e9d8180c787373ac1929bd14ac
Vuceticj <j.vucetic@vegaitsourcing.rs>
2019-10-05 18:12:18 +0200

    Added view models for article container and articles

--- Patrija.Core/ViewModels/Pages/HomeViewModel.cs
+++ Patrija.Core/ViewModels/Pages/HomeViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Patrija.Core.Contexts;
 using Patrija.Core.ViewModels.Partials.Home;
+using Patrija.Core.ViewModels.Shared;
 using Patrija.Models.Generated;
 
 namespace Patrija.Core.ViewModels.Pages
@@ -11,12 +12,16 @@ namespace Patrija.Core.ViewModels.Pages
 		{
 		    var homeIntro = context.Home.HomeIntro.FirstOrDefault();
             HomeIntro = homeIntro != null ? new HomeIntroViewModel(homeIntro) : null;
+            
+            
+            var blogItems = context.Home.Children.OfType<Blog>().First().Children.OfType<BlogItem>().ToArray();
 
-		    var linksList = context.Home.HomeFeaturedLinks;
+            var linksList = context.Home.HomeFeaturedLinks;
 		    LinksList = linksList?.Select(ll => new LinksListViewModel(ll)).ToArray();
 		}
 
         public HomeIntroViewModel HomeIntro { get; }
+        public ArticleContainerViewModel ArticleContainer { get; }
         public LinksListViewModel[] LinksList { get; }
 	}
 }
---
+++ Patrija.Core/ViewModels/Shared/ArticleViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Patrija.Models.Generated;
+using System;
+
+namespace Patrija.Core.ViewModels.Shared
+{
+    public class ArticleViewModel
+    {
+        public ArticleViewModel(BlogItem blogItem)
+        {
+            if (blogItem == null) { throw new ArgumentNullException(nameof(blogItem)); }
+            if (blogItem.BlogItemIntro == null) { throw new ArgumentNullException(nameof(blogItem.BlogItemIntro)); }
+
+            Title = blogItem.BlogItemIntro.BlogIntroTitle;
+            PreviewText = blogItem.BlogItemIntro.BlogIntroPreview;
+            PublishDate = blogItem.BlogItemIntro.BlogIntroDateOfPublishing;
+            Image = new ImageViewModel(blogItem.BlogItemIntro.BlogIntroImage as Image);
+        }
+
+        public string Title { get; }
+        public string PreviewText { get; }
+        public DateTime PublishDate { get; }
+        public ImageViewModel Image { get; }
+    }
+}
