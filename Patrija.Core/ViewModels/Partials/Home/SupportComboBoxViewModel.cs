1f5d353cfc58dec98e948ecb2a8da4e74bc79534
bjelicaluka <bjelicaluka99@gmail.com>
2019-10-05 16:18:10 +0200

    Implemented HomeSupport feature. Need to add Enumerable for button type, and map it on support content box.

---
+++ Patrija.Core/ViewModels/Partials/Home/SupportComboBoxViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Patrija.Core.ViewModels.Shared;
+using Patrija.Models.Generated;
+
+namespace Patrija.Core.ViewModels.Partials.Home
+{
+    public class SupportContentBoxViewModel
+    {
+        public SupportContentBoxViewModel(SupportContentBox supportContentBox)
+        {
+            if (supportContentBox == null) throw new ArgumentNullException(nameof(supportContentBox));
+
+            Title = supportContentBox.SupportContentBoxTitle;
+            Text = supportContentBox.SupportContentBoxText;
+            ButtonColor = supportContentBox.SupportContentBoxButtonColor;
+            Link = new LinkViewModel(supportContentBox.SupportContentBoxButtonUrl);
+        }
+
+        public string Title { get; }
+        public string Text { get;  }
+        public string ButtonColor { get; }
+
+        public LinkViewModel Link { get; }
+    }
+}
