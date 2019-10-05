9755abbca94f487195f9b6bc7eb17d0f75d7ef76
Daniel Kamras <d.kamras@vegaitsourcing.rs>
2019-10-05 12:41:14 +0200

    Home intro implemented

--- Patrija.Core/ViewModels/Pages/HomeViewModel.cs
+++ Patrija.Core/ViewModels/Pages/HomeViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Patrija.Core.Contexts;
+﻿using System.Linq;
+using Patrija.Core.Contexts;
+using Patrija.Core.ViewModels.Partials.Home;
 using Patrija.Models.Generated;
 
 namespace Patrija.Core.ViewModels.Pages
@@ -7,6 +9,10 @@ namespace Patrija.Core.ViewModels.Pages
 	{
 		public HomeViewModel(IPageContext<Home> context) : base(context)
 		{
-		}
+		    var homeIntro = context.Home.HomeIntro.FirstOrDefault();
+            HomeIntro = homeIntro != null ? new HomeIntroViewModel(homeIntro) : null; 
+        }
+
+        public HomeIntroViewModel HomeIntro { get; }
 	}
 }
