72c1c27894c3cc6a1689ca2e57bc7d8c1a30124f
Daniel Kamras <d.kamras@vegaitsourcing.rs>
2019-10-06 13:52:15 +0200

    Contact map info added

--- Patrija.Core/ViewModels/Pages/ContactUsViewModel.cs
+++ Patrija.Core/ViewModels/Pages/ContactUsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Patrija.Core.Contexts;
 using Patrija.Core.ViewModels.Partials.AboutUsPage;
+using Patrija.Core.ViewModels.Partials.ContactUs;
 using Patrija.Core.ViewModels.Partials.Features;
 using Patrija.Models.Generated;
 
@@ -10,16 +11,10 @@ namespace Patrija.Core.ViewModels.Pages
     {
         public ContactUsViewModel(IPageContext<ContactUs> context) : base(context)
         {
-            //var pageIntro = context.Page.AboutUsPageIntro;
-            //PageIntro = pageIntro != null ? new PageIntroViewModel(pageIntro) : null;
-
-            //var featuredProjectsContainer = context.Page.AboutUsFeaturedProjects.FirstOrDefault();
-            //FeaturedProjectsContainer = featuredProjectsContainer != null ? new FeaturedProjectsContainerViewModel(featuredProjectsContainer) : null;
-
-            //var imageWithText = context.Page.AboutUsPageImageWithText.FirstOrDefault();
-            //ImageWithText = imageWithText != null ? new ImageWithTextViewModel(imageWithText) : null;
+            var mapInfo = context.Page.ContactUsMapInfo.FirstOrDefault();
+            MapInfo = mapInfo != null ?  new ContactMapViewModel(mapInfo) : null;
         }
 
-
+        public ContactMapViewModel MapInfo { get; }
     }
 }
