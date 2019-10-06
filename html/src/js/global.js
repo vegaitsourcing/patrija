3badac6b8bdce17660675150c9af57b5ebcad91f
Dejan Boric <d.boric@vegaitsourcing.rs>
2019-10-06 16:55:32 +0200

    added popups

--- html/src/js/global.js
+++ html/src/js/global.js
@@ -2,11 +2,13 @@ import helpers from './_project/helpers';
 import sliders from './_project/sliders';
 import forms from './_project/forms';
 import accordion from "./_project/accordion";
+import popups from "./_project/popups";
 
 $(() => {
 	helpers.init();
 	forms.init();
 	accordion.init();
+	popups.init();
 });
 
 $(window).on('load', () => {
