fa92d5f004c2456715b5625841905a529be05c32
bjelicaluka <bjelicaluka99@gmail.com>
2019-10-06 02:47:44 +0200

    Models updated

--- Patrija.Core/Mappers/BlockMapper.cs
+++ Patrija.Core/Mappers/BlockMapper.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Patrija.Core.ViewModels.Partials.Features;
+using Patrija.Core.ViewModels.Partials.AboutUsPage;
 using Patrija.Core.ViewModels.Shared;
 using Patrija.Models.Generated;
 using Umbraco.Core.Models.PublishedContent;
@@ -24,6 +25,13 @@ namespace Patrija.Core.Mappers
                 {
                     typeof(TextBlock), el => new TextBlockViewModel((TextBlock)el)
                 },
+                {
+                    typeof(TestimonialsContainer), el => new TestimonialsContainerViewModel((TestimonialsContainer)el)
+                },
+                {
+                    typeof(UntaggedFeature), el => new FeatruedProjectViewModel((UntaggedFeature)el)
+                },
+
             };
 
         public static object MapBlock(IPublishedElement blockContent)
