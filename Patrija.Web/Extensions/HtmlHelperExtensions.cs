95d54fca7fc85785683207e389aa53dbac05aeeb
Daniel Kamras <d.kamras@vegaitsourcing.rs>
2019-10-06 18:11:23 +0200

    Moved secure data to separate file

---
+++ Patrija.Web/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Patrija.Web.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlString MultiLineText(this HtmlHelper htmlHelper, string multilineText)
+            => htmlHelper.Raw(multilineText.Replace("\n", "<br />"));
+    }
+}
