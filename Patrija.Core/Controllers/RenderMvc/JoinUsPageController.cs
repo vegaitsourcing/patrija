f28ee29ef875593bb82897d9f74d191ce009dcd2
Daniel Kamras <d.kamras@vegaitsourcing.rs>
2019-10-06 16:09:02 +0200

    Join us page

---
+++ Patrija.Core/Controllers/RenderMvc/JoinUsPageController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+using Patrija.Core.ViewModels.Pages;
+using Patrija.Models.Generated;
+
+namespace Patrija.Core.Controllers.RenderMvc
+{
+    public class JoinUsPageController : BasePageController<JoinUsPage>
+    {
+        public ActionResult Index(JoinUsPage model)
+            => CurrentTemplate(new JoinUsPageViewModel(CreatePageContext(model)));
+    }
+}
