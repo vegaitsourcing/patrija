a36a5ee68c40f7994647ee5b78753732db4a80b4
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2019-10-14 15:12:08 +0200

    Updated hardcoded values

---
+++ Patrija.Core/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrija.Core
+{
+    public static class Constants
+    {
+        public static class Form
+        {
+            public const string DonateFormId = "donations-form";
+            public const string VolunteerFormId = "volunteer-form";
+            public const string UserFormId = "user-form";
+        }
+    }
+}
--- Patrija.Core/UmbracoDictionary.cs
+++ Patrija.Core/UmbracoDictionary.cs
@@ -6,5 +6,10 @@ namespace Patrija.Core
 	public static class UmbracoDictionary
 	{
 		private static UmbracoHelper UmbracoHelper => (UmbracoHelper) Current.Factory.GetInstance(typeof(UmbracoHelper));
-	}
+
+        public static class ScreenReaders
+        {
+            public static string BtnClose => UmbracoHelper.GetDictionaryValue("BtnClose");
+        }
+    }
 }
