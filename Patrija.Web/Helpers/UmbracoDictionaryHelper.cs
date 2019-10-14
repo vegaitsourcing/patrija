67a045b4d238a73ec66d7c0a1a9b8f77b9b1afc7
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2019-10-14 14:46:47 +0200

    Created UmbracoDictionaryHelper.cs

---
+++ Patrija.Web/Helpers/UmbracoDictionaryHelper.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Core.Composing;
+using Umbraco.Web;
+
+namespace Patrija.Web.Helpers
+{
+    public class UmbracoDictionaryHelper
+    {
+        private static UmbracoHelper UmbracoHelper => (UmbracoHelper)Current.Factory.GetInstance(typeof(UmbracoHelper));
+
+        public static class ScreenReaders
+        {
+            public static string BtnClose => UmbracoHelper.GetDictionaryValue("BtnClose");
+        }
+
+    }
+}
