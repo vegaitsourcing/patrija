7d09ec5200367dc5dd884bca8fc3cc152ce450e5
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2019-10-18 16:58:37 +0200

    Update HeaderViewModel; add null checking

--- Patrija.Core/ViewModels/Partials/Layout/HeaderViewModel.cs
+++ Patrija.Core/ViewModels/Partials/Layout/HeaderViewModel.cs
@@ -1,36 +1,35 @@
-﻿using System;
-using Umbraco.Web;
+﻿using Patrija.Core.Contexts;
 using Patrija.Core.ViewModels.Shared;
 using Patrija.Models.Generated;
+using System;
 using System.Linq;
-using Patrija.Core.Contexts;
 
 namespace Patrija.Core.ViewModels.Partials.Layout
 {
-	public class HeaderViewModel
-	{
-		public HeaderViewModel(IPageContext<IPage> context)
-		{
-			if (context == null) throw new ArgumentNullException(nameof(context));
+    public class HeaderViewModel
+    {
+        public HeaderViewModel(IPageContext<IPage> context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
             var header = context.Home as IHeader;
 
-			Logo = new ImageViewModel(header.Logo as Image);
-			LogoUrl = header.AncestorOrSelf<Patrija.Models.Generated.Home>().Url;
-		    JoinUsLink = new LinkViewModel(header.AncestorOrSelf<Patrija.Models.Generated.Home>().JoinUsLink);
+            Logo = header.Logo != null ? new ImageViewModel(header.Logo as Image) : default(ImageViewModel);
+            LogoUrl = context.Home.Url;
+            JoinUsLink = header.JoinUsLink != null ? new LinkViewModel(header.JoinUsLink) : default(LinkViewModel);
 
-            var applicationPages = header.Children.OfType<IPage>().ToArray();
+            var applicationPages = context.Home.Children.OfType<IPage>().ToArray();
             var currentPageUrl = context.CurrentPage.Url;
-            
+
             MenuItems = applicationPages.Prepend(header as IPage).Where(page => !page.UmbracoNaviHide).Select(page => new LinkViewModel(page.Url, page.Name, IsLinkActive(page, currentPageUrl))).ToArray();
-            
-		}
+
+        }
 
         private bool IsLinkActive(IPage page, string currentPageUrl)
-            => page.Url == currentPageUrl;
+            => string.Equals(currentPageUrl, page.Url, StringComparison.OrdinalIgnoreCase);
 
         public ImageViewModel Logo { get; }
-	    public LinkViewModel JoinUsLink { get; }
-	    public string LogoUrl { get; }
+        public LinkViewModel JoinUsLink { get; }
+        public string LogoUrl { get; }
         public LinkViewModel[] MenuItems { get; }
     }
 }
