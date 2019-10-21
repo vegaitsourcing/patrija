4aa2bffe9b113d230dbb349ceed1eafd5b01b7de
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2019-10-21 14:04:24 +0200

    Update NewsListViewModel - remove unnecessary using

--- Patrija.Core/ViewModels/Shared/NewsListViewModel.cs
+++ Patrija.Core/ViewModels/Shared/NewsListViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Patrija.Core.ViewModels.Shared
+﻿namespace Patrija.Core.ViewModels.Shared
 {
     public class NewsListViewModel
     {
