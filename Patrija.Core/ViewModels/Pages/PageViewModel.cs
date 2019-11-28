5ec2dedaf5ed977626d96f04660113ca867120e4
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2019-11-28 16:14:28 +0100

    Updated Home Projects - from NC to MNTP

--- Patrija.Core/ViewModels/Pages/PageViewModel.cs
+++ Patrija.Core/ViewModels/Pages/PageViewModel.cs
@@ -8,42 +8,41 @@ using Patrija.Models.Generated;
 
 namespace Patrija.Core.ViewModels.Pages
 {
-	public class PageViewModel
-	{
-		public PageViewModel(IPageContext<IPage> context)
-		{
-			if (context == null) throw new ArgumentNullException(nameof(context));
+    public class PageViewModel
+    {
+        public PageViewModel(IPageContext<IPage> context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
 
-			MetaTagsLazy = new Lazy<MetaTagsViewModel>(() => new MetaTagsViewModel(context.CreateSeoContext(context.Page)));
-			HeaderLazy = new Lazy<HeaderViewModel>(() => new HeaderViewModel(context));
-			FooterLazy = new Lazy<FooterViewModel>(() => new FooterViewModel(context.Home));
-			GoogleTagManagerScriptCodeLazy = new Lazy<string>(() => context.Home.GoogleTagManagerScriptCode);
-			GoogleTagManagerNonScriptCodeLazy = new Lazy<string>(() => context.Home.GoogleTagManagerNonScriptCode);
-			GoogleAnalyticsCodeLazy = new Lazy<string>(() => context.Home.GoogleAnalyticsScriptCode);
+            MetaTagsLazy = new Lazy<MetaTagsViewModel>(() => new MetaTagsViewModel(context.CreateSeoContext(context.Page)));
+            HeaderLazy = new Lazy<HeaderViewModel>(() => new HeaderViewModel(context));
+            FooterLazy = new Lazy<FooterViewModel>(() => new FooterViewModel(context.Home));
+            GoogleTagManagerScriptCodeLazy = new Lazy<string>(() => context.Home.GoogleTagManagerScriptCode);
+            GoogleTagManagerNonScriptCodeLazy = new Lazy<string>(() => context.Home.GoogleTagManagerNonScriptCode);
+            GoogleAnalyticsCodeLazy = new Lazy<string>(() => context.Home.GoogleAnalyticsScriptCode);
             ShowJoinUsModule = context.Page.ShowJoinUsModule;
-            JoinUsLazy = context.Home.JoinUs != null ? 
-                        new Lazy<JoinUsViewModel>(() =>
-                        new JoinUsViewModel(context.Home.JoinUs.FirstOrDefault())) : 
-                        default(Lazy<JoinUsViewModel>);
+            JoinUs = context.Home.JoinUs != null && context.Home.JoinUs.FirstOrDefault() != null ?
+                     new JoinUsViewModel(context.Home.JoinUs.FirstOrDefault()) :
+                     default(JoinUsViewModel);
         }
 
-		public MetaTagsViewModel MetaTags => MetaTagsLazy.Value;
-		public HeaderViewModel Header => HeaderLazy.Value;
-		public FooterViewModel Footer => FooterLazy.Value;
-		public JoinUsViewModel JoinUs => JoinUsLazy.Value;
-				
-		public string GoogleTagManagerScriptCode => GoogleTagManagerScriptCodeLazy.Value;
-		public string GoogleTagManagerNonScriptCode => GoogleTagManagerNonScriptCodeLazy.Value;
-		public string GoogleAnalyticsCode => GoogleAnalyticsCodeLazy.Value;
+        public MetaTagsViewModel MetaTags => MetaTagsLazy.Value;
+        public HeaderViewModel Header => HeaderLazy.Value;
+        public FooterViewModel Footer => FooterLazy.Value;
+        public JoinUsViewModel JoinUs { get; }
 
-		private Lazy<MetaTagsViewModel> MetaTagsLazy { get; }
-		private Lazy<HeaderViewModel> HeaderLazy { get; }
-		private Lazy<FooterViewModel> FooterLazy { get; }
-		private Lazy<JoinUsViewModel> JoinUsLazy { get; }
-		
-		private Lazy<string> GoogleTagManagerScriptCodeLazy { get; }
-		private Lazy<string> GoogleTagManagerNonScriptCodeLazy { get; }
-		private Lazy<string> GoogleAnalyticsCodeLazy { get; }
+        public string GoogleTagManagerScriptCode => GoogleTagManagerScriptCodeLazy.Value;
+        public string GoogleTagManagerNonScriptCode => GoogleTagManagerNonScriptCodeLazy.Value;
+        public string GoogleAnalyticsCode => GoogleAnalyticsCodeLazy.Value;
+
+        private Lazy<MetaTagsViewModel> MetaTagsLazy { get; }
+        private Lazy<HeaderViewModel> HeaderLazy { get; }
+        private Lazy<FooterViewModel> FooterLazy { get; }
+        private Lazy<JoinUsViewModel> JoinUsLazy { get; }
+
+        private Lazy<string> GoogleTagManagerScriptCodeLazy { get; }
+        private Lazy<string> GoogleTagManagerNonScriptCodeLazy { get; }
+        private Lazy<string> GoogleAnalyticsCodeLazy { get; }
 
         public bool ShowJoinUsModule { get; }
     }
