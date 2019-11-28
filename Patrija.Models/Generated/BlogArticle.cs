4ffc61f77ea8d4855d3e96ed9e3fd733954a8f20
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2019-11-28 12:21:32 +0100

    Add partial class BlogArticle.cs

---
+++ Patrija.Models/Generated/BlogArticle.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Patrija.Models.Generated
+{
+    public partial class BlogArticle
+    {
+        public DateTime CustomDate => !Date.Equals(default(DateTime)) ? Date : CreateDate;
+    }
+}
