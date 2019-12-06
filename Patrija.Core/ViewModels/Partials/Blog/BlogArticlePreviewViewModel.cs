f43d4a53655e6c24a4c17d3127df1ea66e218bb1
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2019-12-06 12:26:23 +0100

    Added missing documents -

---
+++ Patrija.Core/ViewModels/Partials/Blog/BlogArticlePreviewViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Patrija.Core.ViewModels.Shared;
+using Patrija.Models.Generated;
+using System;
+using System.Linq;
+
+namespace Patrija.Core.ViewModels.Partials.Blog
+{
+    public class BlogArticlePreviewViewModel
+    {
+        public BlogArticlePreviewViewModel(BlogArticle blog)
+        {
+            Date = blog.CustomDate;
+
+            Title = blog.BlogArticlePageIntro != null && blog.BlogArticlePageIntro.FirstOrDefault() != null ?
+                    blog.BlogArticlePageIntro.FirstOrDefault().BlogIntroTitle : blog.PageTitle;
+
+            Url = blog.Url;
+
+            Image = blog.BlogArticlePageIntro != null && blog.BlogArticlePageIntro.FirstOrDefault() != null ?
+                    new ImageViewModel(blog.BlogArticlePageIntro.FirstOrDefault().BlogIntroImage.Url) : 
+                    default(ImageViewModel);
+        }
+
+        public DateTime Date { get; }
+        public string Title { get; }
+        public string Url { get; }
+        public ImageViewModel Image { get; }
+    }
+}
---
+++ Patrija.Core/ViewModels/Partials/Blog/BlogWidgetsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Patrija.Models.Generated;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Patrija.Core.ViewModels.Partials.Blog
+{
+    public class BlogWidgetsViewModel
+    {
+        public BlogWidgetsViewModel(BlogWidgets widgets)
+        {
+            Blogs = widgets.Blogs != null ?
+                     widgets.Blogs.OfType<BlogArticle>().Select(b => new BlogArticlePreviewViewModel(b)).ToList() :
+                     default(List<BlogArticlePreviewViewModel>);
+
+            Title = widgets.Title;
+        }
+
+        public List<BlogArticlePreviewViewModel> Blogs { get; }
+        public string Title { get; }
+    }
+}
