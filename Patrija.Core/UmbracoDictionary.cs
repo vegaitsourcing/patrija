237ddb9c3692ff728fbea6d3d6b0ff4ef03084d4
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2019-12-10 11:10:19 +0100

    Add Blog form dictionary values

--- Patrija.Core/UmbracoDictionary.cs
+++ Patrija.Core/UmbracoDictionary.cs
@@ -29,6 +29,19 @@ namespace Patrija.Core
             public static string ReadMore => UmbracoHelper.GetDictionaryValue("Common.ReadMore");
         }
 
+        public static class Blog
+        {
+            public static string CommentsTitle => UmbracoHelper.GetDictionaryValue("Blog.CommentsTitle");
+            public static string CommentFieldError => UmbracoHelper.GetDictionaryValue("Blog.Form.CommentFieldError");
+            public static string CommentFieldText => UmbracoHelper.GetDictionaryValue("Blog.Form.CommentFieldText");
+            public static string EmailFieldError => UmbracoHelper.GetDictionaryValue("Blog.Form.EmailFieldError");
+            public static string EmailFieldText => UmbracoHelper.GetDictionaryValue("Blog.Form.EmailFieldText");
+            public static string NameFieldError => UmbracoHelper.GetDictionaryValue("Blog.Form.NameFieldError");
+            public static string NameFieldText => UmbracoHelper.GetDictionaryValue("Blog.Form.NameFieldText");
+            public static string SubmitButton => UmbracoHelper.GetDictionaryValue("Blog.Form.SubmitButton");
+            public static string SuccessMessage => UmbracoHelper.GetDictionaryValue("Blog.Form.SuccessMessage");
+        }
+
         public static class AboutUs
         {
             public static string CurrentProjects => UmbracoHelper.GetDictionaryValue("AboutUs.CurrentProjects");
